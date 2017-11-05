2a066fb611fdc39099e39409963945369957a088
Figo.Xu <xujh945@qq.com>
2017-11-05 20:27:22 +0800

    python 类继承范例

--- py3/student_teacher.py
+++ py3/student_teacher.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import sys
+from collections import Counter
+
 class Person(object):
     """
     返回具有给定名称的 Person 对象
@@ -14,16 +17,20 @@ class Person(object):
         """
         return self.name
 
+    def get_grade(self):
+        print("UnDefined")
+
 
 class Student(Person):
     """
     返回 Student 对象，采用 name, branch, year 3 个参数
     """
 
-    def __init__(self, name, branch, year):
+    def __init__(self, name, branch, year,score):
         Person.__init__(self, name)
         self.branch = branch
         self.year = year
+        self.score = score
 
     def get_details(self):
         """
@@ -31,23 +38,41 @@ class Student(Person):
         """
         return "{} studies {} and is in {} year.".format(self.name, self.branch, self.year)
 
+    def get_grade(self):
+        data={"Pass":0,"Fail":0}
+        for k,v in Counter(self.score).items() :
+            if k=="A" or k=="B" :
+                data["Pass"] += v
+            else:
+                data["Fail"] += v
+        printMap(data.items())
 
 class Teacher(Person):
     """
     返回 Teacher 对象，采用字符串列表作为参数
     """
-    def __init__(self, name, papers):
+    def __init__(self, name, papers,score):
         Person.__init__(self, name)
         self.papers = papers
+        self.score = score
 
     def get_details(self):
         return "{} teaches {}".format(self.name, ','.join(self.papers))
 
+    def get_grade(self):
+        printMap(Counter(self.score).items())
+        
+def printMap(items) :
+    i=0
+    for k,v in items :
+        print("{}: {}".format(k,v),end=" ")
+        if i<len(items)-1 :
+            print(",",end="")
+        i += 1
 
-person1 = Person('Sachin')
-student1 = Student('Kushal', 'CSE', 2005)
-teacher1 = Teacher('Prashad', ['C', 'C++'])
-
-print(person1.get_details())
-print(student1.get_details())
-print(teacher1.get_details())
+if len(sys.argv) >=3 :
+   tp,score=sys.argv[1],sys.argv[2]
+   if tp=="teacher" :
+       Teacher('', [],score).get_grade()
+   elif tp=="student":
+       Student('', '', 0,score).get_grade()
