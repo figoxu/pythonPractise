e27c93fd153c0734527952c9e9522ddeccfe421b
Figo.Xu <xujh945@qq.com>
2017-11-05 19:10:54 +0800

    增加循环示例

--- py3/hello.py
+++ py3/hello.py
@@ -1,4 +1,37 @@
+#coding=utf-8
 #!/usr/bin/env python3
+
+def ForElse():
+    for i in range(0,10,2):
+        print(i)
+    else:
+        print("Bye Bye")
+
+print('-----ForElse-----')
+ForElse()
+
+def multiplication():
+    print("-"*50)
+    i=1
+    while i<=9:
+        j=1
+        while j<=i:
+            print( "{}X{}={:2d}".format(i,j,i*j) , end=' ')
+            j+=1
+        print()
+        i +=1
+
+print('---multiplication---')
+multiplication()
+
+def fibonacci():
+    a,b = 0,1
+    while b<100:
+        print(b)
+        a,b = b,a+b
+
+print("---fibonacci---")
+fibonacci()
 
 def whileSample():
     n=0
@@ -6,6 +39,8 @@ def whileSample():
         print(n)
         n+=1
 
+
+print("---while---")
 whileSample()
 
 def handleNumber():
@@ -24,6 +59,7 @@ def handleNumber():
     else:
         print("More")
 
+print("---handleNumber---")
 handleNumber()
 
 def aveage():
@@ -33,15 +69,16 @@ def aveage():
         sum=sum+int(input("Enter The {} Number:".format(i)))
     return sum/count
 
+print("---average---")
 avg = aveage()
 print(avg)
 print("Average = {:.2f}".format(avg))
 
 data = {"Figo","JianHui","Xu"}
 nick,name,firstName = data
-print nick
-print name
-print firstName
+print(nick)
+print(name)
+print(firstName)
 
 
 
