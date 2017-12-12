8f891e5b253ff740a5df773108204d6f4ee87be4
Figo.Xu <xujh945@qq.com>
2017-12-12 23:11:43 +0800

    将税收计算，改成收入计算

--- inHardWay/w1/calculator.py
+++ inHardWay/w1/calculator.py
@@ -3,25 +3,33 @@
 import sys
 
 def calculator(salary):
-    taxBase = salary-3500
-    if taxBase<0:
-        return 0
-    tax = taxRange(taxBase,0,1500,0.03)
-    tax = tax + taxRange(taxBase,1500,4500,0.10)
-    tax = tax + taxRange(taxBase,4500,9000,0.20)
-    tax = tax + taxRange(taxBase,9000,35000,0.25)
-    tax = tax + taxRange(taxBase,35000,55000,0.30)
-    tax = tax + taxRange(taxBase,55000,80000,0.35)
-    tax = tax + taxRange(taxBase,80000,2147483647,0.03)
-    return tax
+    insurance=calInsurance(salary)
+    taxBase=salary-insurance-3500
+    rate,baseTax,stageBase=switchStage(taxBase)
+    totalTax = (taxBase-stageBase)*rate+baseTax
+    income =salary-insurance-totalTax
+    return income
 
-def taxRange(taxBase,lower,upper,rate):
-    if taxBase<lower:
-        return 0
-    elif taxBase>upper:
-        return (upper-lower)*rate
+def switchStage(income):
+    if income<=1500:
+        return 0.03,0,0
+    elif income<=4500:
+        return 0.10,105,1500
+    elif income<=9000:
+        return 0.20,555,4500
+    elif income<=35000:
+        return 0.25,1005,9000
+    elif income<=55000:
+        return 0.30,2755,35000
+    elif income<=80000:
+        return 0.35,5505,55000
     else:
-        return (taxBase-lower)*rate
+        return 0.45,13505,80000
+
+
+def calInsurance(salary):
+    return salary*(0.08+0.02+0.005+0+0+0.06)
+
 
 
 try:
