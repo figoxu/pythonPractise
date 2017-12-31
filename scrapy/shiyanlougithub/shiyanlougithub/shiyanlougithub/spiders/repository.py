9193debe3cb102c0b3257fde97bf382c818ec8a3
Figo.Xu <xujh945@qq.com>
2017-12-31 10:12:22 +0800

    Implement Splider

--- scrapy/shiyanlougithub/shiyanlougithub/shiyanlougithub/spiders/repository.py
+++ scrapy/shiyanlougithub/shiyanlougithub/shiyanlougithub/spiders/repository.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from shiyanlougithub.items import ShiyanlougithubItem
+
 
 class RepositorySpider(scrapy.Spider):
     name = 'repository'
-    allowed_domains = ['github.com']
-    start_urls = ['http://github.com/']
+
+    @property
+    def start_urls(self):
+        url_tmpl = 'https://github.com/shiyanlou?page={}&tab=repositories'
+        return (url_tmpl.format(i) for i in range(1, 5))
 
     def parse(self, response):
-        pass
+        for repository in response.css("li.public"):
+            item = ShiyanlougithubItem({
+                    "name": repository.xpath(".//a[@itemprop='name codeRepository']/text()").re_first("\n +(.+)"),
+                    "update_time": repository.xpath(".//relative-time//@datetime").extract_first(),
+            })
+            yield item
+
