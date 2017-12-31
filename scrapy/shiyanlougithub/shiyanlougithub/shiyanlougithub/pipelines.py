9515915a54e8983737a46bb86f0a3bc8eff80959
Figo.Xu <xujh945@qq.com>
2017-12-31 10:26:02 +0800

    Storage Data From PipeLine To Database

--- scrapy/shiyanlougithub/shiyanlougithub/shiyanlougithub/pipelines.py
+++ scrapy/shiyanlougithub/shiyanlougithub/shiyanlougithub/pipelines.py
@@ -5,7 +5,26 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from sqlalchemy.orm import sessionmaker
+
+from shiyanlougithub.items import ShiyanlougithubItem
+from shiyanlougithub.models import Repository,engine
+import time
+
 
 class ShiyanlougithubPipeline(object):
     def process_item(self, item, spider):
+        item['update_time'] = time.strptime('2017-12-28T13:44:59Z', "%Y-%m-%dT%H:%M:%SZ")
+        self.session.add(ShiyanlougithubItem(**item))
         return item
+
+
+    def open_spider(self,spider):
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+
+    def close_spider(self,spider):
+        self.session.commit()
+        self.session.close()
+
+
